3b5a68a2af024d64f2c960e4daaf038d4499747f
@au712952 <202205767@post.au.dk>
2026-02-03 11:27:13 +0100

    cmt

--- AarhusSpaceProgram.Api/obj/Debug/net10.0/AarhusSpaceProgram.Api.AssemblyInfo.cs
+++ AarhusSpaceProgram.Api/obj/Debug/net10.0/AarhusSpaceProgram.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76ecf9eb78673b5725e8cd156d44efb47301c98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ada735480864474301c25e4bc546bce99b7ec93")]
 [assembly: System.Reflection.AssemblyProductAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AarhusSpaceProgram.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
