13cbbe1dec75ff8428359fc017427a0300e2c66e
@au712952 <202205767@post.au.dk>
2026-02-03 18:13:27 +0100

    inserted dummy data into db

---
+++ db_insertions.sql
@@ -0,0 +1,119 @@
+USE AarhusSpaceProgram;
+GO
+
+
+-- Insert Staff (10)
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (1, 'Jane Doe', '2020-03-10');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (2, 'John Smith', '2019-11-20');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (3, 'Maria Gomez', '2021-07-01');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (4, 'Alex Ivanov', '2022-01-25');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (5, 'Susan Lee', '2020-09-12');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (6, 'Omar Hassan', '2023-04-18');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (7, 'Emily Chen', '2021-02-08');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (8, 'David Wilson', '2022-08-30');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (9, 'Sarah Kim', '2019-05-05');
+INSERT INTO Staff (StaffID, EmployeeName, HiringDate) VALUES (10, 'Michael Brown', '2023-10-01');
+GO 
+
+
+-- Insert Departments
+INSERT INTO Department (DepartmentID, DepartmentName) VALUES (1, 'Mission Control');
+INSERT INTO Department (DepartmentID, DepartmentName) VALUES (2, 'Research & Development');
+INSERT INTO Department (DepartmentID, DepartmentName) VALUES (3, 'Astronaut Corps');
+GO
+
+
+-- Define Managers (2)
+INSERT INTO Manager (ManagerID, DepartmentID) VALUES (1, 1); 
+INSERT INTO Manager (ManagerID, DepartmentID) VALUES (2, 2); 
+GO
+
+
+-- Define Astronauts (5)
+INSERT INTO Astronaut (AstronautID, Rank, Paygrade, ExpSimHours, ExpSpaceHours)
+VALUES (3, 'Commander', 'O5', 2500, 1200); 
+INSERT INTO Astronaut (AstronautID, Rank, Paygrade, ExpSimHours, ExpSpaceHours)
+VALUES (4, 'Pilot', 'O4', 1800, 800);     
+INSERT INTO Astronaut (AstronautID, Rank, Paygrade, ExpSimHours, ExpSpaceHours)
+VALUES (5, 'Flight Engineer', 'O3', 1500, 500);  
+INSERT INTO Astronaut (AstronautID, Rank, Paygrade, ExpSimHours, ExpSpaceHours)
+VALUES (8, 'Mission Specialist', 'O3', 1000, 300); 
+INSERT INTO Astronaut (AstronautID, Rank, Paygrade, ExpSimHours, ExpSpaceHours)
+VALUES (10, 'Pilot', 'O4', 1900, 750);    
+GO
+
+
+-- Define Scientists (3)
+INSERT INTO Scientist (ScientistID, Title, Specialty)
+VALUES (6, 'Astrophysicist', 'Exoplanets');
+INSERT INTO Scientist (ScientistID, Title, Specialty)
+VALUES (7, 'Planetary Geologist', 'Martian Geology'); 
+INSERT INTO Scientist (ScientistID, Title, Specialty)
+VALUES (9, 'Exobiologist', 'Astrochemistry');
+GO
+
+
+-- Insert Astro Objects (4)
+INSERT INTO AstroObject (AstroObjectID, AstroObjectName, DistanceValue, BodyType) VALUES (101, 'Mars', 225000000, 'Planet');
+INSERT INTO AstroObject (AstroObjectID, AstroObjectName, DistanceValue, BodyType) VALUES (102, 'Jupiter', 778000000, 'Planet');
+INSERT INTO AstroObject (AstroObjectID, AstroObjectName, DistanceValue, BodyType) VALUES (103, 'Phobos', 9377, 'Moon');
+INSERT INTO AstroObject (AstroObjectID, AstroObjectName, DistanceValue, BodyType) VALUES (104, 'Europa', 671000, 'Moon');
+GO
+
+
+-- Define Planets (2)
+INSERT INTO Planet (PlanetID, PlanetType) VALUES (101, 'RockyPlanet');
+INSERT INTO Planet (PlanetID, PlanetType) VALUES (102, 'GasGiant');
+GO
+
+
+-- Define Moons
+INSERT INTO Moon (MoonID, PlanetID) VALUES (103, 101);
+INSERT INTO Moon (MoonID, PlanetID) VALUES (104, 102);
+GO
+
+
+-- Insert Rockets (2)
+INSERT INTO Rocket (RocketID, ModelName, PayloadCapacity, CrewCapacity, NumberOfStages, FuelCapacity, RocketWeight)
+VALUES (201, 'Falcon Heavy', 63800, 0, 2, 430000, 1420788);
+INSERT INTO Rocket (RocketID, ModelName, PayloadCapacity, CrewCapacity, NumberOfStages, FuelCapacity, RocketWeight)
+VALUES (202, 'Starship', 100000, 100, 2, 1200000, 5000000); 
+GO
+
+
+-- Insert Launchpads (2)
+INSERT INTO Launchpad (LaunchpadID, LaunchpadLocation, MaxSupportedWeight, CurrentStatus)
+VALUES (301, 'Cape Canaveral, LC-39A', 5000000, 'Operational');
+INSERT INTO Launchpad (LaunchpadID, LaunchpadLocation, MaxSupportedWeight, CurrentStatus)
+VALUES (302, 'Vandenberg Space Force Base, SLC-4E', 2000000, 'Maintenance');
+GO
+
+
+-- Insert Missions (3)
+INSERT INTO Mission (MissionID, MissionName, MissionType, MissionStatus, PlannedDuration, PlannedLaunchDate, LaunchpadID, RocketID, ManagerID)
+VALUES (401, 'Mars Reconnaissance Orbiter 2', 'Orbit', 'Planned', 730, '2026-07-20', 301, 201, 1); 
+
+INSERT INTO Mission (MissionID, MissionName, MissionType, MissionStatus, PlannedDuration, PlannedLaunchDate, LaunchpadID, RocketID, ManagerID)
+VALUES (402, 'Jupiter Moon Lander', 'Landing', 'Active', 1095, '2025-03-10', 301, 202, 2);
+
+INSERT INTO Mission (MissionID, MissionName, MissionType, MissionStatus, PlannedDuration, PlannedLaunchDate, LaunchpadID, RocketID, ManagerID)
+VALUES (403, 'Phobos Sample Return', 'Landing', 'Planned', 500, '2027-01-05', 302, 201, 1);
+GO
+
+
+-- Requirement: 1 mission where there is 2+ astronauts
+INSERT INTO MissionAstronaut (MissionID, AstronautID) VALUES (401, 3); 
+INSERT INTO MissionAstronaut (MissionID, AstronautID) VALUES (401, 4); 
+INSERT INTO MissionAstronaut (MissionID, AstronautID) VALUES (401, 5);
+GO 
+-- Other missions needs atronauts too 
+INSERT INTO MissionAstronaut (MissionID, AstronautID) VALUES (402, 8); -- David Wilson on Mission 402
+INSERT INTO MissionAstronaut (MissionID, AstronautID) VALUES (403, 10); -- Michael Brown on Mission 403
+GO
+
+
+-- Requirement: 1 scientists works at at least 2 missions + all missions need a scientist (my own assumption)
+INSERT INTO MissionScientist (MissionID, ScientistID) VALUES (401, 6);
+INSERT INTO MissionScientist (MissionID, ScientistID) VALUES (402, 7); 
+INSERT INTO MissionScientist (MissionID, ScientistID) VALUES (403, 6); 
+GO
---
+++ todo.md
@@ -0,0 +1,36 @@
+TODO made from the assignment description
+
+1. [x] Create an E/R diagram
+
+2. [x] Implement the database schema in SQL
+
+3. Insert data
+   [x] 5 astronauts
+   [x] 3 scientists
+   [x] 2 managers
+   [x] 2 rockets
+   [x] 2 launchpads
+
+   4 celesial bodies (astro objects):
+   [x] 2 planets
+   [x] 2 moons
+
+   [x] 3 missions
+
+   [x] At least one mission must have more than 2 astronauts
+   [x] Atleast one scientist must be attached to more than one mission.
+
+4. Query data
+   Mission overview including:
+   [] mission name
+   [] launch date
+   [] manager name
+   [] rocket model
+   [] launchpad location
+   [] target celestial body name
+
+   [] Mission crew list: astronauts, scientists, manager attached to a mission
+   [] Active or upcoming missions
+   [] Astronauts per mission
+   [] A list of all missions and the number of astronauts assigned to each mission
+   [] Total amount of weight launched from a specific launchpad
